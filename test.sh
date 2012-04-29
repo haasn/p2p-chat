37a35a8a3c34b9cb98d51e716fb01466d5e84a3e
nand <nand@lavabit.com>
2012-04-29 12:01:02 +0200

    Moved .o and .hi files to extra dir

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-ghc src/Math.hs src/Util.hs src/Types.hs src/P2P.hs src/Instances.hs src/Test.hs -o Test && ./Test
+ghc -hidir o -odir o src/Math.hs src/Util.hs src/Types.hs src/P2P.hs src/Instances.hs src/Test.hs -o Test && ./Test
